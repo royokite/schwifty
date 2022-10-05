965cdc7090dcc76fc6686844a3bd39ad3ca88f0f
RoyOkite <royokite@gmail.com>
2022-10-05 22:34:44 +0300

    Added Tailwind CSS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Home() {
-    return <section>Home!</section>
+    return (
+        <section>
+            <h1 className="text-3xl font-bold underline">Rick and Morty</h1>
+            <h3>Series Info</h3>
+            <p>TV Network:	Cartoon Network</p>
+            <p>Premiere Date:	Dec 2, 2013</p>
+            <p>Genre:	Action</p>
+            <p>Executive producers:	Dan Harmon, Justin Roiland</p>
+            <p>
+                After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's doorstep to move in with 
+                her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn't as happy about the family reunion.
+                Jerry is concerned about Rick, a sociopathic scientist, using the garage as his personal laboratory. In the lab,
+                Rick works on a number of sci-fi gadgets, some of which could be considered dangerous. But that's not all Rick does
+                that concerns Jerry. He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.
+            </p>
+        </section>
+    )
 }
 
 export default Home;
