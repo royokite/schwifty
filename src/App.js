3d6eef3a3fce6f36e0decee2f7fd481c9b183807
RoyOkite <royokite@gmail.com>
2022-10-05 19:09:42 +0300

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./components/Home";
+import Characters from "./components/Characters";
+import Episodes from "./components/Episodes";
+import Locations from "./components/Locations";
+
 
 function App() {
   return (
     <section>
       <Home />
+      <Characters />
+      <Locations />
+      <Episodes />
     </section>
   );
 }
