726396450519b515400b67932c0a6d301140bf22
RoyOkite <royokite@gmail.com>
2022-10-05 19:17:58 +0300

    Added a Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Home from "./components/Home";
 import Characters from "./components/Characters";
 import Episodes from "./components/Episodes";
 import Locations from "./components/Locations";
-
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <section>
+      <NavBar />
       <Home />
       <Characters />
       <Locations />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <section>
+            <a href="#">Home</a>
+            <a href="#">Characters</a>
+            <a href="#">Locations</a>
+            <a href="#">Episodes</a>
+        </section>
+    )
+}
+
+export default NavBar;
