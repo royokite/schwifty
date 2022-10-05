694d9fced3891aa68d1fff7841c077060e3094a2
RoyOkite <royokite@gmail.com>
2022-10-05 19:57:43 +0300

    Added Page Logo and Icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom"
 function NavBar() {
     return (
         <section>
+            <img id="logo" src="logo.png" alt="page logo" style={{width: '10rem', height: '10rem'}} />
             <NavLink to="/" exact="true" >Home</NavLink>
             <NavLink to="/characters" exact="true" >Characters</NavLink>
             <NavLink to="/locations" exact="true" >Locations</NavLink>
