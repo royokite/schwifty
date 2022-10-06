ec4ea92da656f3c36b3fa49e4f5fcab4f0f4f4fe
RoyOkite <royokite@gmail.com>
2022-10-06 16:12:03 +0300

    Styled Landing Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import Carousel from "./Carousel";
+import CarouselContainer from "./Carousel";
 
 function Home() {
     return (
-        <section>
-            <h1 className="text-3xl font-bold underline text-lime-400/100">Rick and Morty</h1>
-            <Carousel />
-            <h3>Series Info</h3>
-            <p>TV Network:	Cartoon Network</p>
-            <p>Premiere Date:	Dec 2, 2013</p>
-            <p>Genre:	Action</p>
-            <p>Executive producers:	Dan Harmon, Justin Roiland</p>
-            <p>
-                After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's doorstep to move in with 
-                her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn't as happy about the family reunion.
-                Jerry is concerned about Rick, a sociopathic scientist, using the garage as his personal laboratory. In the lab,
-                Rick works on a number of sci-fi gadgets, some of which could be considered dangerous. But that's not all Rick does
-                that concerns Jerry. He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.
-            </p>
+        <section className="m-5">
+            <article>
+                <h3 className="text-lime-400/100 text-3xl underline">Series Info</h3> <br />
+                <p className="text-lg"><span className="text-lime-400/100">TV Networks: </span>Cartoon Network, Adult Swim</p>
+                <p className="text-lg"><span className="text-lime-400/100">Premiere Date: </span>Dec 2, 2013</p>
+                <p className="text-lg"><span className="text-lime-400/100">Genre: </span>Action</p>
+                <p className="text-lg"><span className="text-lime-400/100">Executive producers:	</span>Dan Harmon, Justin Roiland</p>
+            </article> <br />
+            <article> 
+                <span className="text-lime-400/100 text-2xl underline">Synopsis</span> <br /> <br />
+                <p className="text-justify text-xl bg-lime-400 italic text-black rounded-md p-4"> 
+                    After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's doorstep to move in with 
+                    her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn't as happy about the family reunion.
+                    Jerry is concerned about Rick, a sociopathic scientist, using the garage as his personal laboratory. In the lab,
+                    Rick works on a number of sci-fi gadgets, some of which could be considered dangerous. But that's not all Rick does
+                    that concerns Jerry. He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.
+                </p>
+            </article> <br />
+            <article>
+                <CarouselContainer />   
+            </article>
         </section>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,19 @@ import { NavLink } from "react-router-dom"
 
 function NavBar() {
     return (
-        <nav>
-            <article style={{display: 'inline-block'}}>
-                <img id="logo" src="logo.png" alt="page logo" style={{width: '5rem', height: '5rem'}} />  
+        <nav className="bg-gray-800  mt-0 sticky w-full z-10 top-0 flex flex-wrap">
+            <article  className="flex-none">
+                <img id="logo" src="logo.png" alt="page logo" style={{width: '8rem', height: '8rem'}} />  
             </article>
-            <article style={{float: 'right'}}>
-                <NavLink to="/" exact="true" className="bg-lime-500">Home</NavLink>
-                <NavLink to="/characters" exact="true" className="bg-lime-500">Characters</NavLink>
-                <NavLink to="/locations" exact="true" className="bg-lime-500">Locations</NavLink>
-                <NavLink to="/episodes" exact="true" className="bg-lime-500">Episodes</NavLink>
+            <article className="mt-5 flex-auto">
+                <h1 className="text-5xl text-center font-bold underline text-lime-400/100">Rick and Morty</h1><br /> 
+            </article>
+            
+            <article  className="mt-5 flex-none">
+                <NavLink to="/" exact="true" className="bg-lime-500 m-2 p-2 rounded-md">Home</NavLink>
+                <NavLink to="/characters" exact="true" className="bg-lime-500 m-2 p-2 rounded-md">Characters</NavLink>
+                <NavLink to="/locations" exact="true" className="bg-lime-500 m-2 p-2 rounded-md">Locations</NavLink>
+                <NavLink to="/episodes" exact="true" className="bg-lime-500 m-2 p-2 rounded-md">Episodes</NavLink>
             </article>
         </nav>
     )
